45760f5653c69aae6e1501c75f7efe86bb2a2cc2
vojin-nisevic <nisevic.vojin@gmail.com>
2022-12-04 20:37:50 +0100

    added code books in elite war team controller

--- backend/src/main/java/com/big/backend/controllers/EwteamController.java
+++ backend/src/main/java/com/big/backend/controllers/EwteamController.java
@@ -1,5 +1,6 @@
 package com.big.backend.controllers;
 
+import com.big.backend.models.AllianceRank;
 import com.big.backend.modelsDto.ElWarTeamsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -60,4 +61,32 @@ public class EwteamController {
         elWarTeamService.deleteEwteam(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    //region CODEBOOKS
+
+    @GetMapping(path = "/alliance-rank")
+    public ResponseEntity<?> getAllyRank(){
+        var res = elWarTeamService.getAllyRanks().toArray();
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/front-row")
+    public ResponseEntity<?> getFrontrow(){
+        var res = elWarTeamService.getFrontRow();
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/back-row")
+    public ResponseEntity<?> getBackRow(){
+        var res = elWarTeamService.getBackRow();
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/merit-rank")
+    public ResponseEntity<?> getMeritRank() {
+        var res = elWarTeamService.getMeritRank();
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    //endregion
 }
--- backend/src/main/java/com/big/backend/services/ElWarTeamService.java
+++ backend/src/main/java/com/big/backend/services/ElWarTeamService.java
@@ -1,15 +1,13 @@
 package com.big.backend.services;
 
 import com.big.backend.exception.RequestCustomException;
-import com.big.backend.models.Player;
+import com.big.backend.models.*;
 import com.big.backend.modelsDto.ElWarTeamsDto;
-import com.big.backend.repositories.PlayerRepository;
+import com.big.backend.repositories.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.big.backend.models.ElWarTeam;
-import com.big.backend.repositories.ElWarTeamRepository;
 
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
@@ -20,24 +18,33 @@ import java.util.stream.Collectors;
 public class ElWarTeamService {
     private final ElWarTeamRepository elWarTeamRepository;
     private final PlayerRepository playerRepository;
+    private final AllianceRankRepository allianceRankRepository;
+    private final FrontRowRepository frontRowRepository;
+    private final BackRowRepository backRowRepository;
+    private final MeritRankRepository meritRankRepository;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Autowired
-    public ElWarTeamService(ElWarTeamRepository elWarTeamRepository, PlayerRepository playerRepository) {
+    public ElWarTeamService(ElWarTeamRepository elWarTeamRepository, PlayerRepository playerRepository, AllianceRankRepository allianceRankRepository,
+                            FrontRowRepository frontRowRepository, BackRowRepository backRowRepository, MeritRankRepository meritRankRepository) {
         this.elWarTeamRepository = elWarTeamRepository;
         this.playerRepository = playerRepository;
+        this.allianceRankRepository = allianceRankRepository;
+        this.frontRowRepository = frontRowRepository;
+        this.backRowRepository = backRowRepository;
+        this.meritRankRepository = meritRankRepository;
     }
 
-    public List<ElWarTeam> getAll(){
+    public List<ElWarTeam> getAll() {
         return elWarTeamRepository.findAllByOrderById();
     }
 
     public List<ElWarTeamsDto> getAllWithPlayers() {
         var result = new ArrayList<ElWarTeamsDto>();
         var teamsList = elWarTeamRepository.findAllByOrderById();
-        for (ElWarTeam team: teamsList
-             ) {
+        for (ElWarTeam team : teamsList
+        ) {
             var players = playerRepository.findPlayerByewTeam(team).size();
             result.add(new ElWarTeamsDto(team, players));
         }
@@ -49,38 +56,83 @@ public class ElWarTeamService {
         return elWarTeamRepository.save(elWarTeam);
     }
 
-    public ElWarTeam updateEwteam(ElWarTeam elWarTeam){
-        if (elWarTeamRepository.findEwteamById(elWarTeam.getId()) == null){
+    public ElWarTeam updateEwteam(ElWarTeam elWarTeam) {
+        if (elWarTeamRepository.findEwteamById(elWarTeam.getId()) == null) {
             throw new RequestCustomException("There is no Elite War team with name " + elWarTeam.getName(), 404);
         }
         try {
             return elWarTeamRepository.save(elWarTeam);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             LOGGER.info(e.getCause().getMessage());
             throw e;
         }
     }
 
-    public void deleteEwteam(Long id){
+    public void deleteEwteam(Long id) {
         ElWarTeam team = elWarTeamRepository.findEwteamById(id);
-        if (team != null){
+        if (team != null) {
             elWarTeamRepository.delete(team);
-        }
-        else {
+        } else {
             throw new RequestCustomException("Elite war team you wanted to delete doesn't exist.", 404);
         }
     }
 
-    public ElWarTeam findTeamById(Long id){
+    public ElWarTeam findTeamById(Long id) {
         ElWarTeam team = elWarTeamRepository.findEwteamById(id);
-        if (team != null){
+        if (team != null) {
             return team;
-        }
-        else {
+        } else {
             throw new RequestCustomException("Elite war team you requested doesn't exist.", 404);
         }
     }
 
     //add
+
+    /**
+     * returns all alliance ranks
+     *
+     * @return
+     */
+    public List<AllianceRank> getAllyRanks() {
+        try {
+            return allianceRankRepository.findAll();
+        } catch (Exception e) {
+            throw new RequestCustomException(e.getMessage(), 500);
+        }
+    }
+
+    /**
+     * returns front rows
+     *
+     * @return
+     */
+    public List<FrontRow> getFrontRow() {
+        try {
+            return frontRowRepository.findAll();
+        } catch (Exception e) {
+            throw new RequestCustomException(e.getMessage(), 500);
+        }
+    }
+
+    /**
+     * returns all back rows
+     *
+     * @return
+     */
+    public List<BackRow> getBackRow() {
+        try {
+            return backRowRepository.findAll();
+        } catch (Exception e) {
+            throw new RequestCustomException(e.getMessage(), 500);
+        }
+    }
+
+    public List<MeritRank> getMeritRank() {
+        try {
+            return meritRankRepository.findAll();
+        }
+        catch (Exception e) {
+            throw new RequestCustomException(e.getMessage(), 500);
+        }
+    }
 }
